05745e68bd883b85eac5e0dafe9d29d9dbe93fd2
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-05-01 13:49:18 -0300

    test: ensure CreateUserUseCase calls FindUserByGitHubRepository with correct github account

--- src/modules/user/domain/useCases/tests/create.spec.ts
+++ src/modules/user/domain/useCases/tests/create.spec.ts
@@ -3,8 +3,19 @@ import { AlreadyExistsError } from '@shared/errors/already-exists';
 
 import { User } from '../../entities/user';
 import { IFindUserByEmailRepository } from '../../repositories/find-by-email';
+import { IFindUserByGitHubRepository } from '../../repositories/find-by-github';
 import { CreateUserUseCase } from '../create';
 import { fakeUser } from './mocks/fake-user';
+
+function makeFindUserByGitHubRepositoryStub(): IFindUserByGitHubRepository {
+  class FindUserByGitHubRepositoryStub implements IFindUserByGitHubRepository {
+    async findByGitHub(githubAccount: string): Promise<User | null> {
+      return null;
+    }
+  }
+
+  return new FindUserByGitHubRepositoryStub();
+}
 
 function makeFindUserByEmailRepositoryStub(): IFindUserByEmailRepository {
   class FindUserByEmailRepositoryStub implements IFindUserByEmailRepository {
@@ -18,13 +29,18 @@ function makeFindUserByEmailRepositoryStub(): IFindUserByEmailRepository {
 
 interface SutTypes {
   findUserByEmailRepositoryStub: IFindUserByEmailRepository;
+  findUserByGitHubRepositoryStub: IFindUserByGitHubRepository;
   sut: CreateUserUseCase;
 }
 
 function makeSut(): SutTypes {
   const findUserByEmailRepositoryStub = makeFindUserByEmailRepositoryStub();
-  const sut = new CreateUserUseCase(findUserByEmailRepositoryStub);
-  return { sut, findUserByEmailRepositoryStub };
+  const findUserByGitHubRepositoryStub = makeFindUserByGitHubRepositoryStub();
+  const sut = new CreateUserUseCase(
+    findUserByEmailRepositoryStub,
+    findUserByGitHubRepositoryStub
+  );
+  return { sut, findUserByEmailRepositoryStub, findUserByGitHubRepositoryStub };
 }
 
 const validParams = {
@@ -40,15 +56,36 @@ const validParams = {
 describe('Create user', () => {
   test('Should call FindUserByEmailRepository with correct values', async () => {
     const { sut, findUserByEmailRepositoryStub } = makeSut();
-    const findUserByEmailRepositorySpy = jest.spyOn(findUserByEmailRepositoryStub, 'findByEmail');
+    const findUserByEmailRepositorySpy = jest.spyOn(
+      findUserByEmailRepositoryStub,
+      'findByEmail'
+    );
     await sut.execute(validParams);
-    expect(findUserByEmailRepositorySpy).toHaveBeenCalledWith('any_mail@mail.com');
+    expect(findUserByEmailRepositorySpy).toHaveBeenCalledWith(
+      'any_mail@mail.com'
+    );
   });
 
   test('Should throw AlreadyExitsError if email already exists', async () => {
     const { sut, findUserByEmailRepositoryStub } = makeSut();
-    jest.spyOn(findUserByEmailRepositoryStub, 'findByEmail').mockResolvedValueOnce(fakeUser);
+    jest
+      .spyOn(findUserByEmailRepositoryStub, 'findByEmail')
+      .mockResolvedValueOnce(fakeUser);
     const sutPromise = sut.execute(validParams);
-    await expect(sutPromise).rejects.toThrow(new AlreadyExistsError('Email \'any_mail@mail.com\' already exists'));
+    await expect(sutPromise).rejects.toThrow(
+      new AlreadyExistsError('Email "any_mail@mail.com" already exists')
+    );
+  });
+
+  test('Should call FindUserByGitHubRepository with correct values', async () => {
+    const { sut, findUserByGitHubRepositoryStub } = makeSut();
+    const findUserByGitHubRepositorySpy = jest.spyOn(
+      findUserByGitHubRepositoryStub,
+      'findByGitHub'
+    );
+    await sut.execute(validParams);
+    expect(findUserByGitHubRepositorySpy).toHaveBeenCalledWith(
+      'any_github_account'
+    );
   });
 });
