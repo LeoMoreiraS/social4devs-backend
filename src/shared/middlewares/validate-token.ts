9ef78903f6a1b63b3eb01928f494515e1d1ea960
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-06-04 20:39:40 -0300

    feat: add middleware to validate login token

---
+++ src/shared/middlewares/validate-token.ts
@@ -0,0 +1,15 @@
+import { Response, Request, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { AppError } from '@shared/errors/app-error';
+
+export function verifyToken(request: Request, response: Response, next: NextFunction) {
+  const { token } = request.headers;
+
+  if (!token) {
+    throw new AppError('Token is required');
+  }
+  const secret = process.env.JWT_SECRET ?? 'secret';
+  verify(token.toString(), secret);
+  next();
+}
