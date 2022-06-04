c2cef512a4577f9a2fa87a218ca79158542c76fe
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-06-04 18:18:03 -0300

    refactor: add async to encrypt method

--- src/modules/user/infra/adapters/bcrypt-encrypter-adapter.ts
+++ src/modules/user/infra/adapters/bcrypt-encrypter-adapter.ts
@@ -5,7 +5,7 @@ import { IEncrypterAdapter } from '@user/domain/adapters/encrypter';
 const salt = 10;
 
 export class BcryptEncrypterAdapter implements IEncrypterAdapter {
-  encrypt(password: string): Promise<string> {
+  async encrypt(password: string): Promise<string> {
     const encryptedPassword = bcrypt.hash(password, salt);
     return encryptedPassword;
   }
