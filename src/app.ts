07534723992714812a7a67a109cc1eaabc752f34
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-06-04 17:38:49 -0300

    feat: return app error status code and message in error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import dotenv from 'dotenv';
-import express, { NextFunction, Response, Request } from 'express';
+import express, { Response, Request } from 'express';
 import 'express-async-errors';
 
+import { AppError } from '@shared/errors/app-error';
 import { router } from '@shared/infra/http/routes';
 
 dotenv.config();
@@ -12,7 +13,13 @@ app.use(express.json());
 
 app.use(router);
 
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+app.use((err: Error, request: Request, response: Response) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
   return response.status(500).json({
     status: 'error',
     message: `Internal server error - ${err.message}`,
