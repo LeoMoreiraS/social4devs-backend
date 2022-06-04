dfd397c54911a36e2104615a21230d5e712ad67e
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-06-04 13:05:49 -0300

    feat: create database tables when server starts

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,10 @@
+import { createDatabaseTables } from 'sql/create-database-tables';
+
+import { pgQuery } from '@shared/infra/database/connection';
+
 import { app } from './app';
 
-app.listen(3333, () => console.log('Server is running'));
+app.listen(3333, () => {
+  pgQuery.query(createDatabaseTables);
+  console.log('Server is running');
+});
