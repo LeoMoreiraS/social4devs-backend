bc5a471dd8d166d1de9ca16e5bb8bc370b335194
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-06-04 22:46:39 -0300

    feat: add update user route

--- src/modules/user/infra/http/user.routes.ts
+++ src/modules/user/infra/http/user.routes.ts
@@ -1,14 +1,19 @@
 import { Router } from 'express';
 
+import { verifyToken } from '@shared/middlewares/validate-token';
+
 import { AuthenticateUserController } from '@user/controllers/authenticate-user-controller';
 import { CreateUserController } from '@user/controllers/create-user-controller';
+import { UpdateUserController } from '@user/controllers/update-user-controller';
 
 const userRoutes = Router();
 
 const createUserController = new CreateUserController();
 const authenticateUserController = new AuthenticateUserController();
+const updateUserController = new UpdateUserController();
 
 userRoutes.post('/', createUserController.handle);
+userRoutes.put('/', verifyToken, updateUserController.handle);
 userRoutes.post('/login', authenticateUserController.handle);
 
 export { userRoutes };
