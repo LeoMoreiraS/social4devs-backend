a2533486598c9f82651758f471caa6a0a8b18d37
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-06-07 14:41:36 -0300

    fix: call correct controller in /user/profile

--- src/modules/user/infra/http/user.routes.ts
+++ src/modules/user/infra/http/user.routes.ts
@@ -4,6 +4,7 @@ import { AuthorizationMiddleware } from '@shared/middlewares/authorization-middl
 
 import { AuthenticateUserController } from '@user/controllers/authenticate-user-controller';
 import { CreateUserController } from '@user/controllers/create-user-controller';
+import { FindUserProfileController } from '@user/controllers/find-user-by-email-controller';
 import { GetUserInfoController } from '@user/controllers/get-user-info-controller';
 import { SearchUsersController } from '@user/controllers/search-users-controller';
 import { UpdateUserController } from '@user/controllers/update-user-controller';
@@ -17,12 +18,13 @@ const authenticateUserController = new AuthenticateUserController();
 const updateUserController = new UpdateUserController();
 const getUserInfoController = new GetUserInfoController();
 const findUserController = new SearchUsersController();
+const findUserProfileController = new FindUserProfileController();
 
 userRoutes.post('/', createUserController.handle);
 userRoutes.post('/login', authenticateUserController.handle);
 userRoutes.put('/', authorizationMiddleware.verifyToken, updateUserController.handle);
 userRoutes.get('/', authorizationMiddleware.verifyToken, getUserInfoController.handle);
 userRoutes.get('/search', authorizationMiddleware.verifyToken, findUserController.handle);
-userRoutes.get('/profile', authorizationMiddleware.verifyToken, findUserController.handle);
+userRoutes.get('/profile', authorizationMiddleware.verifyToken, findUserProfileController.handle);
 
 export { userRoutes };
