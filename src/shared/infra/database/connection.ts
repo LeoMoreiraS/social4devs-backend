3f550005cd2bbc7f053b6560f309c76bd6822b7b
leonardo.sepulveda <leonardo.sepulveda@precato.com.br>
2022-06-07 15:02:01 -0300

    fix: connection

--- src/shared/infra/database/connection.ts
+++ src/shared/infra/database/connection.ts
@@ -2,28 +2,28 @@ import dotenv from 'dotenv';
 import { Pool, QueryResult } from 'pg';
 
 dotenv.config();
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export async function query(query: string): Promise<QueryResult> {
-  const client = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: 5432,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
+  const client = await pool.connect();
 
   try {
-    await client.connect();
     const response = await client.query(query);
-    client.end();
+    client.release();
 
     return response;
   } catch (error) {
     console.log(error);
-    client.end();
+    client.release();
     throw error;
   }
 }
