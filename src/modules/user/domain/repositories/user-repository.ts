25558982e277f89bdf1583f58787ae23ebe17d71
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-06-07 10:03:36 -0300

    feat: add search users feature

--- src/modules/user/domain/repositories/user-repository.ts
+++ src/modules/user/domain/repositories/user-repository.ts
@@ -1,4 +1,5 @@
 import { CreateUserDTO } from './dtos/create-user-dto';
+import { FindAllUsersByNameOrNicknameDTO } from './dtos/find-all-by-name-or-nickname-dto';
 import { FindUserByEmailDTO } from './dtos/find-user-by-email-dto';
 import { FindUserByGithubDTO } from './dtos/find-user-by-github-dto';
 import { UpdateUserDTO } from './dtos/update-user-dto';
@@ -7,5 +8,8 @@ export interface IUserRepository {
   create(params: CreateUserDTO.Params): Promise<CreateUserDTO.Result>;
   findByEmail({ email }: FindUserByEmailDTO.Params): Promise<FindUserByEmailDTO.Result>;
   findByGithub({ githubAccount }: FindUserByGithubDTO.Params): Promise<FindUserByGithubDTO.Result>;
+  findAllByNameOrNickname(
+    params: FindAllUsersByNameOrNicknameDTO.Params
+  ): Promise<FindAllUsersByNameOrNicknameDTO.Result>;
   update(params: UpdateUserDTO.Params): Promise<UpdateUserDTO.Result>;
 }
--- src/modules/user/infra/http/user.routes.ts
+++ src/modules/user/infra/http/user.routes.ts
@@ -5,6 +5,7 @@ import { AuthorizationMiddleware } from '@shared/middlewares/authorization-middl
 import { AuthenticateUserController } from '@user/controllers/authenticate-user-controller';
 import { CreateUserController } from '@user/controllers/create-user-controller';
 import { GetUserInfoController } from '@user/controllers/get-user-info-controller';
+import { SearchUsersController } from '@user/controllers/search-users-controller';
 import { UpdateUserController } from '@user/controllers/update-user-controller';
 
 const userRoutes = Router();
@@ -15,10 +16,12 @@ const createUserController = new CreateUserController();
 const authenticateUserController = new AuthenticateUserController();
 const updateUserController = new UpdateUserController();
 const getUserInfoController = new GetUserInfoController();
+const findUserController = new SearchUsersController();
 
 userRoutes.post('/', createUserController.handle);
 userRoutes.post('/login', authenticateUserController.handle);
 userRoutes.put('/', authorizationMiddleware.verifyToken, updateUserController.handle);
 userRoutes.get('/', authorizationMiddleware.verifyToken, getUserInfoController.handle);
+userRoutes.get('/search', authorizationMiddleware.verifyToken, findUserController.handle);
 
 export { userRoutes };
