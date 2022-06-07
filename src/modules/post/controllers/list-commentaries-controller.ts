48c063fca6f84477d1f102cd4aa1869548bd150b
leonardo.sepulveda <leonardo.sepulveda@precato.com.br>
2022-06-07 11:47:09 -0300

    headers

--- src/modules/post/controllers/list-commentaries-controller.ts
+++ src/modules/post/controllers/list-commentaries-controller.ts
@@ -5,15 +5,15 @@ import { CommentaryRepository } from '@post/infra/repositories/commentary-reposi
 
 export class ListCommentariesController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { postEmail, postBody } = request.body;
+    const { postEmail, postBody } = request.headers;
 
     const commentaryRepository = new CommentaryRepository();
 
     const createCommentaryUseCase = new ListCommentariesUseCase(commentaryRepository);
 
     const result = await createCommentaryUseCase.execute({
-      postEmail,
-      postBody,
+      postEmail: postEmail?.toString() ?? '',
+      postBody: postBody?.toString() ?? '',
     });
 
     return response.status(201).json(result);
