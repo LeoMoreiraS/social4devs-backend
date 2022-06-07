f942f4e6431c4da4217f7811377b365a66c8ff22
ArthurEnrique15 <arthurenrique1512@gmail.com>
2022-06-07 09:39:57 -0300

    refactor: change find user to get user info

--- src/modules/user/infra/http/user.routes.ts
+++ src/modules/user/infra/http/user.routes.ts
@@ -4,7 +4,7 @@ import { AuthorizationMiddleware } from '@shared/middlewares/authorization-middl
 
 import { AuthenticateUserController } from '@user/controllers/authenticate-user-controller';
 import { CreateUserController } from '@user/controllers/create-user-controller';
-import { FindUserWithJoinsController } from '@user/controllers/find-user-controller';
+import { GetUserInfoController } from '@user/controllers/get-user-info-controller';
 import { UpdateUserController } from '@user/controllers/update-user-controller';
 
 const userRoutes = Router();
@@ -14,11 +14,11 @@ const authorizationMiddleware = new AuthorizationMiddleware();
 const createUserController = new CreateUserController();
 const authenticateUserController = new AuthenticateUserController();
 const updateUserController = new UpdateUserController();
-const findUserWithJoinsController = new FindUserWithJoinsController();
+const getUserInfoController = new GetUserInfoController();
 
 userRoutes.post('/', createUserController.handle);
 userRoutes.post('/login', authenticateUserController.handle);
 userRoutes.put('/', authorizationMiddleware.verifyToken, updateUserController.handle);
-userRoutes.get('/', authorizationMiddleware.verifyToken, findUserWithJoinsController.handle);
+userRoutes.get('/', authorizationMiddleware.verifyToken, getUserInfoController.handle);
 
 export { userRoutes };
