1ead758159845c5903a2bd249b886efbc8c38404
leonardo.sepulveda <leonardo.sepulveda@precato.com.br>
2022-06-07 00:12:50 -0300

    chore add port

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { migrations } from '@shared/infra/database/migrations';
 
 import { app } from './app';
 
-app.listen(3333, async () => {
+app.listen(process.env.PORT || 3333, async () => {
   await query(migrations);
   console.log('Server is running');
 });
